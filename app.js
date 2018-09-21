38353e4fd4e74fe93d05d6fb454434e28d4329dc
Luis Felipe J. Higginson <newluifogoo@gmail.com>
2018-09-21 02:14:57 -0400

    template engine

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ var app = express();
 var port = process.env.PORT || 3000;
 //middleware to map static files
 app.use('/assets', express.static(__dirname + '/public'));
+//template engine
+app.set('view engine', 'ejs');
 //routing
 app.get('/', function(req, res){
-    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h3>Hello body! ...</h3></body></html>');
+    /*res.send(`<!DOCTYPE html><html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    
+                </head>
+                <body>
+                    <h3>Hello body! ...</h3>
+                <link href=assets/style.css type=text/css rel=stylesheet/>
+                </body>
+                </html>`);*/
+    res.render('index');
+});
+app.get('/person/:id', function(req, res){
+    res.render('person', { ID: req.params.id});
 });
 app.get('/api', function(req, res){
     res.json( { firstname: 'John', lastname: 'Doe' } );
